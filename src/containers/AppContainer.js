da3f589550a4fbbf49b2bc61dde40c75c9c57062
tranthithuhan <tranthithuhan@gmail.com>
2019-10-09 23:48:34 +0200

    init project

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,57 +1,9 @@
 import React, {Component} from 'react';
-import {withRouter, Route, Switch, Redirect} from 'react-router';
-import {Layout, Icon} from 'antd';
-import AppMenu from "../components/AppMenu";
-import {MAIN_PAGE_ROUTE, PRODUCT_ITEM_PAGE_ROUTE, PRODUCT_PAGE_ROUTE} from "../index";
-import ProductList from "./ProductList";
-import ProductItem from "./ProductItem";
+import {withRouter} from 'react-router';
 
 export class AppContainer extends Component {
-    state = {
-        siderIsCollapsed: true,
-    };
-
-    toggle = () => {
-        this.setState({
-            siderIsCollapsed: !this.state.siderIsCollapsed,
-        });
-    };
-
-    goTo = (route) => {
-        this.props.history.push(route);
-    };
-
     render() {
-        return (
-            <Layout className="app-container container">
-                <AppMenu currentRoute={this.props.location.pathname}
-                         siderIsCollapsed={this.state.siderIsCollapsed}
-                         onClick={this.goTo}/>
-
-                <Layout>
-                    <Layout.Header style={{background: '#fff', padding: 0}}>
-                        <Icon
-                            className="trigger"
-                            type={this.state.siderIsCollapsed ? 'right' : 'left'}
-                            onClick={this.toggle}
-                        />
-                    </Layout.Header>
-                    <Layout.Content>
-                        <Switch>
-                            <Route exact key={MAIN_PAGE_ROUTE} path={MAIN_PAGE_ROUTE} render={() =>
-                                <div>
-                                    <div className="app-logo"/>
-                                    <h1>Hello</h1>
-                                </div>
-                            }/>
-                            <Route exact key={PRODUCT_PAGE_ROUTE} path={PRODUCT_PAGE_ROUTE} component={ProductList}/>
-                            <Route exact key={PRODUCT_ITEM_PAGE_ROUTE} path={PRODUCT_ITEM_PAGE_ROUTE} component={ProductItem}/>
-                            <Redirect to={MAIN_PAGE_ROUTE}/>
-                        </Switch>
-                    </Layout.Content>
-                </Layout>
-            </Layout>
-        );
+        return (<div>hello</div>);
     }
 }
 
