5e07f08b547f105b7059925575200a400abaa2b4
tranthithuhan <tranthithuhan@gmail.com>
2019-10-10 07:15:50 +0200

    add product list page

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,9 +1,57 @@
 import React, {Component} from 'react';
-import {withRouter} from 'react-router';
+import {withRouter, Route, Switch, Redirect} from 'react-router';
+import {Layout, Icon} from 'antd';
+import AppMenu from "../components/AppMenu";
+import {MAIN_PAGE_ROUTE, PRODUCT_ITEM_PAGE_ROUTE, PRODUCT_PAGE_ROUTE} from "../index";
+import ProductList from "./ProductList";
+import ProductItem from "./ProductItem";
 
 export class AppContainer extends Component {
+    state = {
+        siderIsCollapsed: true,
+    };
+
+    toggle = () => {
+        this.setState({
+            siderIsCollapsed: !this.state.siderIsCollapsed,
+        });
+    };
+
+    goTo = (route) => {
+        this.props.history.push(route);
+    };
+
     render() {
-        return (<div>hello</div>);
+        return (
+            <Layout className="app-container container">
+                <AppMenu currentRoute={this.props.location.pathname}
+                         siderIsCollapsed={this.state.siderIsCollapsed}
+                         onClick={this.goTo}/>
+
+                <Layout>
+                    <Layout.Header style={{background: '#fff', padding: 0}}>
+                        <Icon
+                            className="trigger"
+                            type={this.state.siderIsCollapsed ? 'right' : 'left'}
+                            onClick={this.toggle}
+                        />
+                    </Layout.Header>
+                    <Layout.Content>
+                        <Switch>
+                            <Route exact key={MAIN_PAGE_ROUTE} path={MAIN_PAGE_ROUTE} render={() =>
+                                <div>
+                                    <div className="app-logo"/>
+                                    <h1>Hello</h1>
+                                </div>
+                            }/>
+                            <Route exact key={PRODUCT_PAGE_ROUTE} path={PRODUCT_PAGE_ROUTE} component={ProductList}/>
+                            <Route exact key={PRODUCT_ITEM_PAGE_ROUTE} path={PRODUCT_ITEM_PAGE_ROUTE} component={ProductItem}/>
+                            <Redirect to={MAIN_PAGE_ROUTE}/>
+                        </Switch>
+                    </Layout.Content>
+                </Layout>
+            </Layout>
+        );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Route, BrowserRouter as Router, Redirect} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import AppContainer from './containers/AppContainer.js';
 
+import configureStore from './reducers/configureStore';
+import reducers from './reducers/configReducers';
+
+import 'antd/dist/antd.css';
 import "./styles/App.css"
 
 export const MAIN_PAGE_ROUTE = "/";
+export const PRODUCT_PAGE_ROUTE = "/products";
+export const PRODUCT_ITEM_PAGE_ROUTE = "/products/:productId";
 
 render((
-    <Router>
-        <div className={"container"}>
-            <Route path={MAIN_PAGE_ROUTE} component={AppContainer} />
-            <Redirect to='/' />
-        </div>
-    </Router>
+    <Provider store={configureStore(reducers)}>
+        <Router>
+            <div className={"container"}>
+                <AppContainer/>
+            </div>
+        </Router>
+    </Provider>
 ), document.getElementById('root'));
