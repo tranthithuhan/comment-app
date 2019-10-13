db0c99003477a74cf5f6f18a4c3d21c05384734a
tranthithuhan <tranthithuhan@gmail.com>
2019-10-13 02:44:07 +0200

    Updates

---
+++ precache-manifest.4845f2d07d4c8d6c6fcefaec59a08445.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b0191486b8cac71b8ce6907ebec691",
+    "url": "/comment-app/index.html"
+  },
+  {
+    "revision": "68c0ec0b7aa2ae4740cc",
+    "url": "/comment-app/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "916015eeddadf93c0649",
+    "url": "/comment-app/static/css/main.1e6a8def.chunk.css"
+  },
+  {
+    "revision": "68c0ec0b7aa2ae4740cc",
+    "url": "/comment-app/static/js/2.7ae5107b.chunk.js"
+  },
+  {
+    "revision": "916015eeddadf93c0649",
+    "url": "/comment-app/static/js/main.41353f95.chunk.js"
+  },
+  {
+    "revision": "3ae000e3aa6a24bf8d40",
+    "url": "/comment-app/static/js/runtime-main.fc77d700.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/comment-app/static/media/logo.25bf045c.svg"
+  }
+]);
